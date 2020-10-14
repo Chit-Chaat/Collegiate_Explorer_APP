a988c0e6e6f8e7cdc6a864669baac459a838e6fe
AaronYang2333 <aaron19940628@gmail.com>
2020-10-14 16:02:20 -0700

    filter button and go search

--- collegiate-explorer-app/src/components/advanced_search_filter.vue
+++ collegiate-explorer-app/src/components/advanced_search_filter.vue
@@ -9,39 +9,50 @@
           <el-tag v-if="this.selectTags.area !== ''" closable size="small" @close="handleClose(0)">
             {{this.selectTags.area}}
           </el-tag>
-          <el-tag v-if="this.selectTags.work !== ''" closable size="small" @close="handleClose(1)">
-            {{this.selectTags.work}}
+          <el-tag v-if="this.selectTags.major !== ''" closable size="small" @close="handleClose(1)">
+            {{this.selectTags.major}}
           </el-tag>
-          <el-tag v-if="this.selectTags.num !== ''" closable size="small" @close="handleClose(2)">
-            {{this.selectTags.num}}
+          <el-tag v-if="this.selectTags.tuition !== ''" closable size="small" @close="handleClose(2)">
+            {{this.selectTags.tuition}}
           </el-tag>
+        </el-col>
+      </el-row>
+      <el-row>
+        <el-col :span="2" style="color: yellowgreen">
+          Area:
+        </el-col>
+        <el-col :span="20">
+          <el-button type="text" v-for="area in areas" :key="area" size="medium" @click="selectKey(area,0)">{{area}}
+          </el-button>
         </el-col>
       </el-row>
       <el-row>
         <el-col :span="2" style="color: yellowgreen">
           Major：
         </el-col>
-        <el-col :span="8">
-          <el-button type="text" v-for="work in works" :key="work" size="medium" @click="selectKey(work,0)">{{work}}
+        <el-col :span="20">
+          <el-button type="text" v-for="major in marjors" :key="major" size="medium" @click="selectKey(major,1)">
+            {{major}}
           </el-button>
         </el-col>
       </el-row>
       <el-row>
         <el-col :span="2" style="color: yellowgreen">
-          省份：
+          Tuition：
         </el-col>
         <el-col :span="20">
-          <el-button type="text" v-for="area in areas" :key="area" size="medium" @click="selectKey(area,1)">{{area}}
+          <el-button type="text" v-for="tuition in tuitions" :key="tuition" size="medium" @click="selectKey(tuition,2)">
+            {{tuition}}
           </el-button>
+          <span style="color: darkgray;">[per semester]</span>
         </el-col>
       </el-row>
       <el-row>
-        <el-col :span="2" style="color: yellowgreen">
-          指数：
-        </el-col>
-        <el-col :span="8">
-          <el-button type="text" v-for="num in nums" :key="num" size="medium" @click="selectKey(num,2)">{{num}}
-          </el-button>
+        <el-col :span="23" style="text-align: right;">
+          <el-button-group>
+            <el-button  icon="el-icon-refresh-right" @click="reset">Rest All</el-button>
+            <el-button @click="go_search">Search<i class="el-icon-search el-icon--right" ></i></el-button>
+          </el-button-group>
         </el-col>
       </el-row>
     </el-card>
@@ -53,6 +64,11 @@
     data() {
       return {
         areas: [
+          'New England', 'Mid East', 'Great Lakes',
+          'Plains', 'Southeast', 'Southwest',
+          'Rocky Mountains', 'Far West', 'Outlying areas'],
+
+        marjors: [
           'Agriculture',
           'Architecture',
           'Arts',
@@ -80,33 +96,46 @@
           'Psychology',
           'Social Sciences'],
 
-        works: ['工业', '旅游业', '餐饮', '农业'],
 
-        nums: ['>=0.9', '>=0.8', '>=0.6'],
+        tuitions: ['>= $5k', '>= $10k', '>= $15k'],
 
         selectTags: {
-          area: 'Computer',
-          work: '',
-          num: '',
+          area: 'Southwest',
+          major: '',
+          tuition: '',
         }
       }
     },
     methods: {
       selectKey(tag, key) {
         if (key === 0)
-          this.selectTags.work = tag
-        else if (key === 1)
           this.selectTags.area = tag
+        else if (key === 1)
+          this.selectTags.major = tag
         else
-          this.selectTags.num = tag
+          this.selectTags.tuition = tag
       },
       handleClose(tag) {
         if (tag === 0)
           this.selectTags.area = ''
         else if (tag === 1)
-          this.selectTags.work = ''
+          this.selectTags.major = ''
         else
-          this.selectTags.num = ''
+          this.selectTags.tuition = ''
+      },
+      reset(e){
+        this.selectTags.area = ''
+        this.selectTags.major = ''
+        this.selectTags.tuition = ''
+        this.$message({
+          message: 'All filters have been cleared.',
+          type: 'success'
+        });
+        e.target.blur() // auto unfocused
+      },
+      go_search(){
+        let text = this.selectTags.area + '/'+ this.selectTags.major +'/'+ this.selectTags.tuition 
+        console.log(text)
       }
     }
   }
